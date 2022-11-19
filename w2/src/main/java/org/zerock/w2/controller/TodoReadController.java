6ac87c8550cf86374d13d6b73feb55dd48f79d97
jhwanl <jjonghlee98@gmail.com>
2022-11-19 16:41:48 +0900

    Cookie tutorial

--- w2/src/main/java/org/zerock/w2/controller/TodoReadController.java
+++ w2/src/main/java/org/zerock/w2/controller/TodoReadController.java
@@ -2,6 +2,7 @@ package org.zerock.w2.controller;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -20,20 +21,63 @@ public class TodoReadController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-
         try {
 
             Long tno = Long.parseLong(req.getParameter("tno"));
             TodoDTO dto = todoService.read(tno);
             req.setAttribute("dto", dto);
+
+            Cookie viewTodoCookie = findCookie(req.getCookies(), "viewTodos");
+            String todoListStr = viewTodoCookie.getValue();
+            boolean exist = false;
+
+            if (todoListStr != null && todoListStr.indexOf(tno + "-") >= 0) {
+                exist = true;
+            }
+
+            log.info(exist);
+
+            if (!exist) {
+                todoListStr += tno + "-";
+                viewTodoCookie.setValue(todoListStr);
+                viewTodoCookie.setPath("/");
+                viewTodoCookie.setMaxAge(60 * 60 * 24);
+                resp.addCookie(viewTodoCookie);
+            }
+
             req.getRequestDispatcher("/WEB-INF/todo/read.jsp").forward(req, resp);
 
         } catch (Exception e) {
 
+            e.printStackTrace();
             log.error(e.getMessage());
             throw new ServletException("read error");
 
         }
 
     }
+
+    private Cookie findCookie(Cookie[] cookies, String cookieName) {
+
+        Cookie targetCookie = null;
+
+        if (targetCookie != null && cookies.length > 0) {
+            for (Cookie ck : cookies) {
+                if (ck.getName().equals(cookieName)) {
+                    targetCookie = ck;
+                    break;
+                }
+            }
+        }
+
+        if (targetCookie == null) {
+            targetCookie = new Cookie(cookieName, "");
+            targetCookie.setPath("/");
+            targetCookie.setMaxAge(60 * 60 * 24);
+        }
+
+        return targetCookie;
+
+    }
+
 }
--- w2/src/main/webapp/WEB-INF/todo/list.jsp
+++ w2/src/main/webapp/WEB-INF/todo/list.jsp
@@ -9,7 +9,7 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
 <head>
-    <title>List</title>
+	<title>List</title>
 </head>
 <body>
 <h1>Todo List</h1>
@@ -17,18 +17,18 @@
 <h3>${loginInfo.mname}</h3>
 
 <ul>
-    <c:forEach var="dto" items="${dto}">
-        <li>
-            <span><a href="/todo/read?tno=${dto.tno}">${dto.tno}</a></span>
-            <span>${dto.title}</span>
-            <span>${dto.dueDate}</span>
-            <span>${dto.finished ? "DONE" : "NOT YET"}</span>
-        </li>
-    </c:forEach>
+	<c:forEach var="dto" items="${dto}">
+		<li>
+			<span><a href="/todo/read?tno=${dto.tno}">${dto.tno}</a></span>
+			<span>${dto.title}</span>
+			<span>${dto.dueDate}</span>
+			<span>${dto.finished ? "DONE" : "NOT YET"}</span>
+		</li>
+	</c:forEach>
 </ul>
 
 <form action="/logout" method="post">
-    <button type="submit">LOGOUT</button>
+	<button type="submit">LOGOUT</button>
 </form>
 </body>
 </html>
